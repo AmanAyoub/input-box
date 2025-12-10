3ae5dbc6c6019feac0057e4e002d8f776a42a625
AmanAyoub <aman.ayoub16@gmail.com>
2025-12-10 13:13:34 +0430

    remove focused class

--- javascripts/script.js
+++ javascripts/script.js
@@ -2,6 +2,11 @@ document.addEventListener("DOMContentLoaded", event => {
   let textField = document.querySelector(".text-field");
 
   textField.addEventListener("click", event => {
+    event.stopPropagation();
     textField.classList.add("focused");
+  });
+
+  document.addEventListener("click", event => {
+    textField.classList.remove("focused");
   });
 });
