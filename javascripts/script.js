34af589490656495d098d70d389876bbd682d13b
AmanAyoub <aman.ayoub16@gmail.com>
2025-12-10 13:26:44 +0430

    remove cursor

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,16 +1,20 @@
 document.addEventListener("DOMContentLoaded", event => {
   let textField = document.querySelector(".text-field");
+  let blinkingCursor;
 
   textField.addEventListener("click", event => {
     event.stopPropagation();
     textField.classList.add("focused");
 
-    setInterval(() => {
+    blinkingCursor = setInterval(() => {
       textField.classList.toggle("cursor");
     }, 500);
   });
 
   document.addEventListener("click", event => {
+    clearInterval(blinkingCursor);
     textField.classList.remove("focused");
+    textField.classList.remove("cursor");
+
   });
 });
